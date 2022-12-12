7b8a48837268635461b2590d8718c362ed16561c
woosuk1103 <woosukbyun0724@gmail.com>
2022-12-12 16:08:44 +0900

    revised

---
+++ examples/cli/run_example.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1  # set to one to prevent numpy to run in parallel
+
+echo 'Getting data'
+hcga -v get_data $1
+
+echo 'Extracting features'
+hcga -v extract_features datasets/$1.pkl -m fast -n 4  --timeout 10.0
+
+echo 'Run classification'
+hcga -v feature_analysis $1
